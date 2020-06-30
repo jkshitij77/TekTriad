f4aaf9dcd0c08aaedf6897c226e4fc805f787c47
shuchitpant <emailshuchitpant@gmail.com>
2020-07-01 02:17:58 +0530

    added the live projects section and made it responsive

--- src/App.js
+++ src/App.js
@@ -39,12 +39,57 @@ function App() {
                 </div>
               </div>
 
+
               {/* Section two here */}
               <div className="section two">
+              
                 <div class="divforsectionotwo">
+                  
+                  <div class="top-part-s2">
+                    <div class="s2-logo-name">Live Projects <img src={require('./attachments/tektriadlogo.svg')} alt="logo"/></div>
+                  </div>
+                  <div class="proj-container">
+                    <div class="proj-container-left">
+                      <div class="proj-container-left-box"><a href=""><img src={require("./attachments/web1.jpg")} /></a>
+                      <p class="proj-txt">Lorem ipsum dolor <br/> sit amet.</p>
+                      </div>
+                      
+                      
+                      <div class="proj-container-left-box">
+                      <a href="">
+                      <img src={require("./attachments/web2.jpg")} /></a>
+                      <p class="proj-txt">Lorem ipsum dolor <br/> sit amet.</p>
 
+                      </div>
+                      
+                      
+                    </div>
+                    <div class="proj-container-right">
+                    
+                      <div class="proj-container-right-box">
+                      <a href="">   <img src={require("./attachments/web30.jpg")} /> </a>
+                      <p class="proj-txt">Lorem ipsum dolor <br/> sit amet.</p>
+
+                      </div>
+                      
+                      
+                      <div class="proj-container-right-box">
+                      <a href="">    <img src={require("./attachments/web4.jpg")} /> </a>
+                      <p class="proj-txt">Lorem ipsum dolor <br/> sit amet.</p>
+                      </div>
+                      
+
+                    </div>
+
+                  </div>
+                  
+                
                 </div>
+
               </div>
+
+
+
               <div className="section three">
                 <div class="divforsectionothree">
                   <div class="lowerthingservices">
@@ -76,7 +121,7 @@ function App() {
               <div className="section four">
 
               <div class="aboutus-cont">
-              <img class="Abt" src={require("./attachments/aboutus.svg")} />
+              <img class="Abt" viewBox="-100 -100 100 100" src={require("./attachments/aboutus.svg")} />
               <p class="aboutustxt"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, et! Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, accusantium! Lorem ipsum dolor sit amet consectetur consectetur adipisicing.</p>
               </div>
 
@@ -85,9 +130,9 @@ function App() {
                 
                   <div class="found">
                     <div class="dabba"><div class="found-ph"><img class="founder" src={require("./attachments/Sharma.jpg")} /></div> 
-                    <div class="found-ph">Chand shabd apne baare me </div></div>
-                    <div class="dabba"><div class="found-ph"><img class="founder" src={require("./attachments/DP.jpg")} /> </div> <div class="found-ph"> Chand milky boy baare me</div></div>
-                    <div class="dabba"><div class="found-ph"><img class="founder" src={require("./attachments/Shuchit.jpg")} /></div> <div class="found-ph">Chand shabd apne baare <br />me</div> </div>
+                    <div class="found-ph">She quirky tho.</div></div>
+                    <div class="dabba"><div class="found-ph"><img class="founder" src={require("./attachments/DP.jpg")} /> </div> <div class="found-ph"> Mikayla tum kyu bhag gayi</div></div>
+                    <div class="dabba"><div class="found-ph"><img class="founder" src={require("./attachments/Shuchit.jpg")} /></div> <div class="found-ph"> Sab ho jaega bhai</div> </div>
 
                   </div>
                  
