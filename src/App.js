e30f0eee10a9042c7dac23d57a42d3ab3de86fec
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-06-30 18:45:43 +0530

    Screens 1 and 3 done

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ function App() {
                     <div class="firstpagebutton">Sign Up</div>
                   </div>
                   <div class="logos">
-                    <a href="https://www.instagram.com/tektriad/"><img src="https://img.icons8.com/metro/208/000000/instagram-new.png" alt="insta"/></a>
-                    <a href=""><img src="https://img.icons8.com/android/208/000000/facebook-new.png" alt="fb"/></a>
-                    <a href=""><img src="https://img.icons8.com/android/208/000000/linkedin.png" alt="linkedin"/></a>
-                    <a href=""><img src="https://img.icons8.com/material-sharp/208/000000/github.png" alt="github"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/tektriad/" ><img src="https://img.icons8.com/metro/208/000000/instagram-new.png" alt="insta"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/tektriad/"><img src="https://img.icons8.com/android/208/000000/facebook-new.png" alt="fb"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/tektriad/"><img src="https://img.icons8.com/android/208/000000/linkedin.png" alt="linkedin"/></a>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/tektriad/"><img src="https://img.icons8.com/material-sharp/208/000000/github.png" alt="github"/></a>
                   </div>
                 </div>
               </div>
@@ -46,6 +46,32 @@ function App() {
                 </div>
               </div>
               <div className="section three">
+                <div class="divforsectionothree">
+                  <div class="lowerthingservices">
+                    <div class="servicecard"> 
+                      <img src={require('./attachments/webdevlogo.png')} alt="webdev"/> 
+                      <h1> Web Development</h1>
+                      <p>Sharma ki maa randi hai. Uska poora khaandaan randi hai. Gazab randi hai. Darua madarchodh</p>
+                    </div>
+                    <div class="servicecard"> 
+                      <img src={require('./attachments/designinglogo.png')} alt="design"/> 
+                      <h1>Designing</h1>
+                      <h1 class="emptyh1"></h1>
+                      <p>Sharma ki maa randi hai. Uska poora khaandaan randi hai. Gazab randi hai. Darua madarchodh</p>
+                    </div>
+                    <div class="servicecard"> 
+                      <img src={require('./attachments/webdevlogo.png')} alt="smm"/> 
+                      <h1> Social Media Marketing</h1>
+                      <p>Sharma ki maa randi hai. Uska poora khaandaan randi hai. Gazab randi hai. Darua madarchodh</p>
+                    </div>
+                    <div class="servicecard"> 
+                      <img src={require('./attachments/seologo.png')} alt="seo"/> 
+                      <h1>SEO</h1>
+                      <h1 class="emptyh1"></h1>
+                      <p>Sharma ki maa randi hai. Uska poora khaandaan randi hai. Gazab randi hai. Darua madarchodh</p>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="section four">
                 <div class="BEHEN">
